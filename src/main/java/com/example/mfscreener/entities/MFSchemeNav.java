62da06db14e8a29c02544bb215de7ad54a6b174d
Raja Kolli <rajadilipkolli@gmail.com>
2022-11-11 06:34:02 +0000

    Upgrade to SpringBoot 3.0.0-RC3

--- src/main/java/com/example/mfscreener/entities/MFSchemeNav.java
+++ src/main/java/com/example/mfscreener/entities/MFSchemeNav.java
@@ -3,7 +3,7 @@ package com.example.mfscreener.entities;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 import java.io.Serializable;
 import java.time.LocalDate;
 
