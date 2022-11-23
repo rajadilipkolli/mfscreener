3f069133dc9adb7295a229434569fc09c5f3c2e3
Raja Kolli <rajadilipkolli@gmail.com>
2022-11-23 10:35:07 +0000

    adds more archunit tests and fixes them

--- src/main/java/com/example/mfscreener/repository/UserSchemeDetailsEntityRepository.java
+++ src/main/java/com/example/mfscreener/repository/UserSchemeDetailsEntityRepository.java
@@ -5,9 +5,12 @@ import com.example.mfscreener.entities.UserSchemeDetailsEntity;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserSchemeDetailsEntityRepository
         extends JpaRepository<UserSchemeDetailsEntity, Long> {
+
     @Query("select distinct(amfi) u from UserSchemeDetailsEntity u")
     List<Long> findDistinctByAmfi();
 }
--- src/test/java/com/example/mfscreener/archunit/ArchitectureConstants.java
+++ src/test/java/com/example/mfscreener/archunit/ArchitectureConstants.java
@@ -9,10 +9,10 @@ public class ArchitectureConstants {
 
     // Packages
     public static final String CONTROLLER_PACKAGE = "..controller..";
-    public static final String MODEL_PACKAGE = "..model..";
+    public static final String MODEL_PACKAGE = "..models..";
     public static final String DTO_PACKAGE = "..dto..";
     public static final String REPOSITORY_PACKAGE = "..repository..";
-    public static final String SERVICE_PACKAGE = "..service..";
+    public static final String SERVICE_PACKAGE = "..service.impl..";
 
     // Package to scan
     public static final String DEFAULT_PACKAGE = "com.example.mfscreener";
