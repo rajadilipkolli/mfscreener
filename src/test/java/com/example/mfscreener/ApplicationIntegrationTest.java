d8806fc0ba807a05f1e86be006b6337e1d5bd994
Raja Kolli <rajadilipkolli@gmail.com>
2023-04-30 00:41:17 +0000

    using new TestContainers

--- src/test/java/com/example/mfscreener/ApplicationIntegrationTest.java
+++ src/test/java/com/example/mfscreener/ApplicationIntegrationTest.java
@@ -1,15 +1,11 @@
 /* Licensed under Apache-2.0 2021-2022. */
 package com.example.mfscreener;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import com.example.mfscreener.common.AbstractIntegrationTest;
 import org.junit.jupiter.api.Test;
 
 class ApplicationIntegrationTest extends AbstractIntegrationTest {
 
     @Test
-    void contextLoads() {
-        assertThat(POSTGRE_SQL_CONTAINER.isRunning()).isTrue();
-    }
+    void contextLoads() {}
 }
