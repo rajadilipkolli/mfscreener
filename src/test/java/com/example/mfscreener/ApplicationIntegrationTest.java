f1e3766a9b99371acc8592129767072b4a5ed74b
Raja Kolli <rajadilipkolli@gmail.com>
2023-04-30 13:55:39 +0000

    Fixes spotless issue

--- src/test/java/com/example/mfscreener/ApplicationIntegrationTest.java
+++ src/test/java/com/example/mfscreener/ApplicationIntegrationTest.java
@@ -1,11 +1,11 @@
 /* Licensed under Apache-2.0 2021-2023. */
 package com.example.mfscreener;
 
-import com.example.mfscreener.common.AbstractIntegrationTest;
-import org.junit.jupiter.api.Test;
-
 import static com.example.mfscreener.TestcontainersConfig.LOKI_CONTAINER;
 import static org.assertj.core.api.Assertions.assertThat;
+
+import com.example.mfscreener.common.AbstractIntegrationTest;
+import org.junit.jupiter.api.Test;
 
 class ApplicationIntegrationTest extends AbstractIntegrationTest {
 
