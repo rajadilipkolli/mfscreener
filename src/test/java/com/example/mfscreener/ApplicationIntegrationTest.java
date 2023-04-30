cbc72e7269bfe51bc71c62753cf8d9a35f7493f3
Raja Kolli <rajadilipkolli@gmail.com>
2023-04-30 14:15:03 +0000

    using docker compose module to run tests

--- src/test/java/com/example/mfscreener/ApplicationIntegrationTest.java
+++ src/test/java/com/example/mfscreener/ApplicationIntegrationTest.java
@@ -1,16 +1,11 @@
 /* Licensed under Apache-2.0 2021-2023. */
 package com.example.mfscreener;
 
-import static com.example.mfscreener.TestcontainersConfig.LOKI_CONTAINER;
-import static org.assertj.core.api.Assertions.assertThat;
-
 import com.example.mfscreener.common.AbstractIntegrationTest;
 import org.junit.jupiter.api.Test;
 
 class ApplicationIntegrationTest extends AbstractIntegrationTest {
 
     @Test
-    void contextLoads() {
-        assertThat(LOKI_CONTAINER.isRunning()).isTrue();
-    }
+    void contextLoads() {}
 }
