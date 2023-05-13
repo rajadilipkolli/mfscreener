fee5bb056e337ede2259ba0b44d957a92650dfaf
Raja Kolli <rajadilipkolli@gmail.com>
2023-05-13 20:32:45 +0530

    Implement XIRR Service (#164)
    * Initial commit of XIRR Service
    
    * adds Junit for XIRRTest
    
    * Fixes ArchUnit Violation
    
    * fixes ArchUnit Violations
    
    * start tempo for junits to pass
    
    * adds loki to the build
    
    * adds integrationTest to circleCI
    
    * fixes issue with CircleCI sript
    
    * fixes gradle version
    
    * attempt to fix CircleCI Script
    
    * Updated config.yml
    
    * Updated config.yml
    
    * Revert back to original script
    
    * Fixes Build Issue
    
    * remvoes loki container
    
    * fixes devcontainer issue
    
    * recreate devcontainer from scratch
    
    * fixes spotlessIssue

---
+++ src/main/java/com/example/mfscreener/service/impl/XIRRService.java
@@ -0,0 +1,86 @@
+/* Licensed under Apache-2.0 2023. */
+package com.example.mfscreener.service.impl;
+
+import com.example.mfscreener.models.Transaction;
+import com.example.mfscreener.models.TransactionType;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import org.apache.commons.math3.analysis.interpolation.LinearInterpolator;
+import org.apache.commons.math3.analysis.polynomials.PolynomialSplineFunction;
+import org.apache.commons.math3.analysis.solvers.BrentSolver;
+import org.apache.commons.math3.analysis.solvers.UnivariateSolver;
+import org.springframework.stereotype.Service;
+
+@Service
+public class XIRRService {
+
+    private static final double ACCURACY = 0.000001;
+    private static final int MAX_EVALUATIONS = 1000;
+
+    public double calculateXIRR(List<Transaction> cashflowsList) {
+        List<Transaction> buys = new ArrayList<>();
+        List<Transaction> redeems = new ArrayList<>();
+
+        for (Transaction cashflow : cashflowsList) {
+            if (cashflow.getType() == TransactionType.BUY) {
+                buys.add(cashflow);
+            } else if (cashflow.getType() == TransactionType.REDEEM) {
+                redeems.add(cashflow);
+            }
+        }
+
+        buys.sort(Comparator.comparing(Transaction::getDate));
+        redeems.sort(Comparator.comparing(Transaction::getDate));
+
+        double[] values = new double[cashflowsList.size()];
+        LocalDate startDate = null;
+        LocalDate endDate = null;
+
+        for (int i = 0; i < buys.size(); i++) {
+            Transaction buy = buys.get(i);
+            values[i] = -buy.getAmount();
+            if (startDate == null || buy.getDate().isBefore(startDate)) {
+                startDate = buy.getDate();
+            }
+            if (endDate == null || buy.getDate().isAfter(endDate)) {
+                endDate = buy.getDate();
+            }
+        }
+
+        for (int i = 0; i < redeems.size(); i++) {
+            Transaction redeem = redeems.get(i);
+            values[i + buys.size()] = redeem.getAmount();
+            if (startDate == null || redeem.getDate().isBefore(startDate)) {
+                startDate = redeem.getDate();
+            }
+            if (endDate == null || redeem.getDate().isAfter(endDate)) {
+                endDate = redeem.getDate();
+            }
+        }
+
+        long daysBetween = ChronoUnit.DAYS.between(startDate, endDate);
+
+        double[] times = new double[cashflowsList.size()];
+
+        for (int i = 0; i < buys.size(); i++) {
+            Transaction buy = buys.get(i);
+            long daysSinceStart = ChronoUnit.DAYS.between(startDate, buy.getDate());
+            times[i] = (double) daysSinceStart / daysBetween;
+        }
+
+        for (int i = 0; i < redeems.size(); i++) {
+            Transaction redeem = redeems.get(i);
+            long daysSinceStart = ChronoUnit.DAYS.between(startDate, redeem.getDate());
+            times[i + buys.size()] = (double) daysSinceStart / daysBetween;
+        }
+
+        PolynomialSplineFunction function = new LinearInterpolator().interpolate(times, values);
+        UnivariateSolver solver = new BrentSolver(ACCURACY);
+        double guess = 0.1;
+
+        return solver.solve(MAX_EVALUATIONS, function, 0.0, 1.0, guess);
+    }
+}
--- src/test/java/com/example/mfscreener/common/AbstractTestContainerBase.java
+++ src/test/java/com/example/mfscreener/common/AbstractTestContainerBase.java
@@ -1,9 +1,8 @@
-/* Licensed under Apache-2.0 2021-2022. */
+/* Licensed under Apache-2.0 2021-2023. */
 package com.example.mfscreener.common;
 
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.lifecycle.Startables;
@@ -17,11 +16,8 @@ public class AbstractTestContainerBase {
             .withUsername("username")
             .withPassword("password");
 
-    protected static final GenericContainer LOKI_CONTAINER =
-            new GenericContainer<>("grafana/loki").withExposedPorts(3100);
-
     static {
-        Startables.deepStart(POSTGRE_SQL_CONTAINER, LOKI_CONTAINER).join();
+        Startables.deepStart(POSTGRE_SQL_CONTAINER).join();
     }
 
     @DynamicPropertySource
@@ -29,9 +25,5 @@ public class AbstractTestContainerBase {
         propertyRegistry.add("spring.datasource.url", POSTGRE_SQL_CONTAINER::getJdbcUrl);
         propertyRegistry.add("spring.datasource.username", POSTGRE_SQL_CONTAINER::getUsername);
         propertyRegistry.add("spring.datasource.password", POSTGRE_SQL_CONTAINER::getPassword);
-        propertyRegistry.add(
-                "application.loki.url",
-                () -> "http://" + LOKI_CONTAINER.getHost() + ":" + LOKI_CONTAINER.getMappedPort(3100)
-                        + "/loki/api/v1/push");
     }
 }
