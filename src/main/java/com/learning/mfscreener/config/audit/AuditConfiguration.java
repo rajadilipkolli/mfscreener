ce1c5ca95ea6463552ad693511ecc1e522175b49
rajadilipkolli <rajadilipkolli@gmail.com>
2023-07-01 00:43:58 +0530

    enables JPA Auditing

---
+++ src/main/java/com/learning/mfscreener/config/audit/AuditConfiguration.java
@@ -0,0 +1,17 @@
+/* Licensed under Apache-2.0 2021-2022. */
+package com.learning.mfscreener.config.audit;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration(proxyBeanMethods = false)
+@EnableJpaAuditing
+public class AuditConfiguration {
+
+    @Bean
+    public AuditorAware<String> auditorAware() {
+        return new EntityAuditorAware();
+    }
+}
---
+++ src/main/java/com/learning/mfscreener/config/audit/EntityAuditorAware.java
@@ -0,0 +1,13 @@
+/* Licensed under Apache-2.0 2021-2022. */
+package com.learning.mfscreener.config.audit;
+
+import java.util.Optional;
+import org.springframework.data.domain.AuditorAware;
+
+public class EntityAuditorAware implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.of("Raja");
+    }
+}
