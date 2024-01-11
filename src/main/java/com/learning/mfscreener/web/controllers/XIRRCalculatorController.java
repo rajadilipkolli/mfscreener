691149cae7ee484a4dd74d42da5a16736e53ff3b
Raja Kolli <rajadilipkolli@gmail.com>
2024-01-11 18:39:53 +0000

    fix : circleci script

--- src/main/java/com/learning/mfscreener/web/controllers/XIRRCalculatorController.java
+++ src/main/java/com/learning/mfscreener/web/controllers/XIRRCalculatorController.java
@@ -5,9 +5,11 @@ import java.util.Map;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class XIRRCalculatorController {
 
     private final CalculatorService calculatorService;
