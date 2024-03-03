822a92f3a9ddcfc5997b7392412261b687c3e5f5
Raja Kolli <rajadilipkolli@gmail.com>
2024-03-03 14:44:21 +0000

    declare restclient Bean

--- src/main/java/com/learning/mfscreener/config/RestTemplateConfiguration.java
+++ src/main/java/com/learning/mfscreener/config/RestTemplateConfiguration.java
@@ -1,8 +1,10 @@
 package com.learning.mfscreener.config;
 
+import java.time.Duration;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration(proxyBeanMethods = false)
@@ -10,6 +12,14 @@ public class RestTemplateConfiguration {
 
     @Bean
     RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
-        return restTemplateBuilder.build();
+        return restTemplateBuilder
+                .setConnectTimeout(Duration.ofSeconds(30))
+                .setReadTimeout(Duration.ofSeconds(60))
+                .build();
+    }
+
+    @Bean
+    RestClient restClient(RestTemplate restTemplate) {
+        return RestClient.create(restTemplate);
     }
 }
