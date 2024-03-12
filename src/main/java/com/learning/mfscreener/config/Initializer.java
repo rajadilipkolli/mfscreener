941baaf2173095555cfee792ba94c5e09ded0625
Raja Kolli <rajadilipkolli@gmail.com>
2024-03-12 17:31:06 +0000

    feat : handle exception in a scenario where external system is down

--- src/main/java/com/learning/mfscreener/config/Initializer.java
+++ src/main/java/com/learning/mfscreener/config/Initializer.java
@@ -18,6 +18,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StopWatch;
 import org.springframework.util.StringUtils;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
 
 @Component
@@ -33,57 +35,62 @@ public class Initializer implements CommandLineRunner {
     public void run(String... args) throws IOException {
         long start = System.currentTimeMillis();
         log.info("Loading All Funds...");
-        List<MFSchemeDTO> chopArrayList = new ArrayList<>();
-        String allNAVs = restTemplate.getForObject(AppConstants.AMFI_WEBSITE_LINK, String.class);
-        Reader inputString = new StringReader(Objects.requireNonNull(allNAVs));
-        try (BufferedReader br = new BufferedReader(inputString)) {
-            String lineValue = br.readLine();
-            for (int i = 0; i < 4; ++i) {
-                lineValue = br.readLine();
-            }
-            String amc = lineValue;
-            while (lineValue != null) {
-                int check = 0;
-                final String[] tokenize = lineValue.split(AppConstants.SEPARATOR);
-                if (tokenize.length == 1) {
-                    check = 1;
-                    amc = lineValue;
-                }
-                if (check == 0) {
-                    final String schemecode = tokenize[0];
-                    final String payout = tokenize[1];
-                    final String reinvestment = tokenize[2];
-                    final String schemename = tokenize[3];
-                    final String nav = tokenize[4];
-                    final String date = tokenize[5];
-                    final MFSchemeDTO tempObj =
-                            new MFSchemeDTO(amc, Long.valueOf(schemecode), payout, schemename, nav, date);
-                    chopArrayList.add(tempObj);
-                }
-                lineValue = br.readLine();
-                if (!StringUtils.hasText(lineValue)) {
+        try {
+            String allNAVs = restTemplate.getForObject(AppConstants.AMFI_WEBSITE_LINK, String.class);
+            Reader inputString = new StringReader(Objects.requireNonNull(allNAVs));
+            List<MFSchemeDTO> chopArrayList = new ArrayList<>();
+            try (BufferedReader br = new BufferedReader(inputString)) {
+                String lineValue = br.readLine();
+                for (int i = 0; i < 4; ++i) {
                     lineValue = br.readLine();
                 }
+                String amc = lineValue;
+                while (lineValue != null) {
+                    int check = 0;
+                    final String[] tokenize = lineValue.split(AppConstants.SEPARATOR);
+                    if (tokenize.length == 1) {
+                        check = 1;
+                        amc = lineValue;
+                    }
+                    if (check == 0) {
+                        final String schemecode = tokenize[0];
+                        final String payout = tokenize[1];
+                        final String reinvestment = tokenize[2];
+                        final String schemename = tokenize[3];
+                        final String nav = tokenize[4];
+                        final String date = tokenize[5];
+                        final MFSchemeDTO tempObj =
+                                new MFSchemeDTO(amc, Long.valueOf(schemecode), payout, schemename, nav, date);
+                        chopArrayList.add(tempObj);
+                    }
+                    lineValue = br.readLine();
+                    if (!StringUtils.hasText(lineValue)) {
+                        lineValue = br.readLine();
+                    }
+                }
             }
-        }
-        log.info(
-                "All Funds loaded in {} milliseconds, total funds loaded :{}",
-                (System.currentTimeMillis() - start),
-                chopArrayList.size());
+            log.info(
+                    "All Funds loaded in {} milliseconds, total funds loaded :{}",
+                    (System.currentTimeMillis() - start),
+                    chopArrayList.size());
 
-        if (mfSchemesRepository.count() != chopArrayList.size()) {
-            StopWatch stopWatch = new StopWatch();
-            stopWatch.start("saving fundNames");
-            List<MFSchemeEntity> list = new ArrayList<>();
-            List<Long> schemeCodesList = mfSchemesRepository.findAllSchemeIds();
-            chopArrayList.removeIf(s -> schemeCodesList.contains(s.schemeCode()));
-            chopArrayList.forEach(scheme -> {
-                MFSchemeEntity mfSchemeEntity = conversionServiceAdapter.mapMFSchemeDTOToMFSchemeEntity(scheme);
-                list.add(mfSchemeEntity);
-            });
-            mfSchemesRepository.saveAll(list);
-            stopWatch.stop();
-            log.info("saved in db in : {} sec", stopWatch.getTotalTimeSeconds());
+            if (mfSchemesRepository.count() != chopArrayList.size()) {
+                StopWatch stopWatch = new StopWatch();
+                stopWatch.start("saving fundNames");
+                List<MFSchemeEntity> list = new ArrayList<>();
+                List<Long> schemeCodesList = mfSchemesRepository.findAllSchemeIds();
+                chopArrayList.removeIf(s -> schemeCodesList.contains(s.schemeCode()));
+                chopArrayList.forEach(scheme -> {
+                    MFSchemeEntity mfSchemeEntity = conversionServiceAdapter.mapMFSchemeDTOToMFSchemeEntity(scheme);
+                    list.add(mfSchemeEntity);
+                });
+                mfSchemesRepository.saveAll(list);
+                stopWatch.stop();
+                log.info("saved in db in : {} sec", stopWatch.getTotalTimeSeconds());
+            }
+        } catch (HttpClientErrorException | ResourceAccessException httpClientErrorException) {
+            // eating as we can't do much, it should be set when available using Nightly job
+            log.error("Unable to load data from :{}", AppConstants.AMFI_WEBSITE_LINK, httpClientErrorException);
         }
     }
 }
