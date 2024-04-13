22960c0268e5f7074da52aabc6020e91a8d6a9d7
Raja Kolli <rajadilipkolli@gmail.com>
2024-04-13 21:33:46 +0000

    fix : issue with binding variables

--- src/main/java/com/learning/mfscreener/web/controllers/NAVController.java
+++ src/main/java/com/learning/mfscreener/web/controllers/NAVController.java
@@ -23,15 +23,15 @@ public class NAVController implements NAVApi {
 
     @Override
     @GetMapping(path = "/{schemeCode}")
-    public ResponseEntity<MFSchemeDTO> getScheme(@PathVariable Long schemeCode) {
+    public ResponseEntity<MFSchemeDTO> getScheme(@PathVariable("schemeCode") Long schemeCode) {
         return ResponseEntity.ok(navService.getNav(schemeCode));
     }
 
     @Override
     @GetMapping(path = "/{schemeCode}/{date}")
     public ResponseEntity<MFSchemeDTO> getSchemeNavOnDate(
-            @PathVariable Long schemeCode,
-            @PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
+            @PathVariable("schemeCode") Long schemeCode,
+            @PathVariable("date") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
         return ResponseEntity.ok(navService.getNavOnDate(schemeCode, date));
     }
 }
--- src/main/java/com/learning/mfscreener/web/controllers/SchemeController.java
+++ src/main/java/com/learning/mfscreener/web/controllers/SchemeController.java
@@ -22,13 +22,14 @@ public class SchemeController implements SchemeApi {
 
     @Override
     @GetMapping(path = "/{schemeName}")
-    public ResponseEntity<List<FundDetailProjection>> fetchSchemes(@PathVariable String schemeName) {
+    public ResponseEntity<List<FundDetailProjection>> fetchSchemes(@PathVariable("schemeName") String schemeName) {
         return ResponseEntity.ok(schemeService.fetchSchemes(schemeName));
     }
 
     @Override
     @GetMapping(path = "/fund/{fundName}")
-    public ResponseEntity<List<FundDetailProjection>> fetchSchemesByFundName(@PathVariable String fundName) {
+    public ResponseEntity<List<FundDetailProjection>> fetchSchemesByFundName(
+            @PathVariable("fundName") String fundName) {
         return ResponseEntity.ok(schemeService.fetchSchemesByFundName(fundName));
     }
 }
