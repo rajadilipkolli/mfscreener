fd9abe578215e5a0082b0753f259a325cbd5305f
Raja Kolli <rajadilipkolli@gmail.com>
2024-04-14 18:25:21 +0000

    fix : reduce cognitive complexity

--- src/main/java/com/learning/mfscreener/service/HistoricalNavService.java
+++ src/main/java/com/learning/mfscreener/service/HistoricalNavService.java
@@ -53,6 +53,127 @@ public class HistoricalNavService {
     }
 
     public String getHistoricalNav(Long schemeCode, LocalDate navDate) {
+        URI historicalNavUri = buildHistoricalNavUri(navDate);
+        Optional<MFSchemeEntity> bySchemeId = schemeService.findBySchemeCode(schemeCode);
+        String payOut;
+        boolean persistSchemeInfo = false;
+        SchemeNameAndISIN schemeNameAndISIN = null;
+        if (bySchemeId.isEmpty()) {
+            // discontinued Scheme
+            schemeNameAndISIN = fetchSchemeDetails(schemeCode);
+            payOut = schemeNameAndISIN.getIsin();
+            persistSchemeInfo = true;
+        } else {
+            payOut = bySchemeId.get().getPayOut();
+        }
+        return fetchAndProcessNavData(
+                historicalNavUri, payOut, persistSchemeInfo, schemeNameAndISIN, schemeCode, navDate);
+    }
+
+    String fetchAndProcessNavData(
+            URI historicalNavUri,
+            String payOut,
+            boolean persistSchemeInfo,
+            SchemeNameAndISIN schemeNameAndISIN,
+            Long schemeCode,
+            LocalDate navDate) {
+        try {
+            Reader inputString = fetchHistoricalNavByCallingUri(historicalNavUri);
+            return parseNavData(inputString, payOut, persistSchemeInfo, schemeNameAndISIN, schemeCode, navDate);
+        } catch (ResourceAccessException exception) {
+            // eating as we can't do much, and it should be set when available
+            LOGGER.error("Unable to load Historical Data, downstream service is down ", exception);
+            return null;
+        }
+    }
+
+    String parseNavData(
+            Reader inputString,
+            String payOut,
+            boolean persistSchemeInfo,
+            SchemeNameAndISIN schemeNameAndISIN,
+            Long schemeCode,
+            LocalDate navDate) {
+        String oldSchemeId = null;
+        try (BufferedReader br = new BufferedReader(inputString)) {
+            String lineValue = br.readLine();
+            for (int i = 0; i < 2; ++i) {
+                lineValue = br.readLine();
+            }
+            String schemeType = lineValue;
+            String amc = lineValue;
+            while (lineValue != null && !StringUtils.hasText(oldSchemeId)) {
+                String[] tokenize = lineValue.split(AppConstants.NAV_SEPARATOR);
+                boolean nonAmcRow = true;
+                boolean processRowByForce = false;
+                if (tokenize.length == 1) {
+                    nonAmcRow = false;
+                    String tempVal = lineValue;
+                    lineValue = br.readLine();
+                    if (!StringUtils.hasText(lineValue)) {
+                        lineValue = br.readLine();
+                        tokenize = lineValue.split(AppConstants.NAV_SEPARATOR);
+                        if (tokenize.length == 1) {
+                            schemeType = tempVal;
+                            amc = lineValue;
+                        } else {
+                            amc = tempVal;
+                            processRowByForce = true;
+                        }
+                    }
+                }
+                if (nonAmcRow || processRowByForce) {
+                    final String schemecode = tokenize[0];
+                    final String payout = tokenize[2];
+                    if (payOut.equalsIgnoreCase(payout)) {
+                        oldSchemeId = schemecode;
+                        if (persistSchemeInfo) {
+                            String schemename = tokenize[1];
+                            String nav = tokenize[4];
+                            String date = tokenize[7];
+                            final MFSchemeDTO mfSchemeDTO = new MFSchemeDTO(
+                                    amc, Long.valueOf(schemecode), payout, schemename, nav, date, schemeType);
+                            MFSchemeEntity mfSchemeEntity = mfSchemeDtoToEntityMapper.mapMFSchemeDTOToMFSchemeEntity(
+                                    mfSchemeDTO, mfSchemeTypeRepository);
+                            schemeService.saveEntity(mfSchemeEntity);
+                        }
+                    }
+                }
+                lineValue = br.readLine();
+                if (!StringUtils.hasText(lineValue)) {
+                    lineValue = br.readLine();
+                }
+            }
+        } catch (IOException e) {
+            LOGGER.error("Exception Occurred while reading response", e);
+            throw new NavNotFoundException("Unable to parse for %s".formatted(schemeCode), navDate);
+        }
+        if (!StringUtils.hasText(oldSchemeId) && schemeNameAndISIN != null) {
+            // Manually creating entry in mf_scheme table as no entry found in historical link
+            MFSchemeEntity mfSchemeEntity = new MFSchemeEntity();
+            mfSchemeEntity.setPayOut(payOut);
+            mfSchemeEntity.setSchemeId(schemeCode);
+            mfSchemeEntity.setSchemeName(schemeNameAndISIN.getScheme());
+            schemeService.saveEntity(mfSchemeEntity);
+            oldSchemeId = String.valueOf(schemeCode);
+        } else {
+            LOGGER.info("No Nav found for the given day");
+        }
+        return oldSchemeId;
+    }
+
+    StringReader fetchHistoricalNavByCallingUri(URI historicalNavUri) {
+        String allNAVsByDate = restClient.get().uri(historicalNavUri).retrieve().body(String.class);
+        return new StringReader(Objects.requireNonNull(allNAVsByDate));
+    }
+
+    SchemeNameAndISIN fetchSchemeDetails(Long schemeCode) {
+        return userSchemeDetailsService
+                .findFirstBySchemeCode(schemeCode)
+                .orElseThrow(() -> new SchemeNotFoundException("Fund with schemeCode " + schemeCode + " Not Found"));
+    }
+
+    URI buildHistoricalNavUri(LocalDate navDate) {
         // URL https://portal.amfiindia.com/DownloadNAVHistoryReport_Po.aspx?tp=1&frmdt=01-Jan-2024&todt=03-Jan-2024
         String toDate = navDate.format(FORMATTER_DD_MMM_YYYY);
         String fromDate = navDate.minusDays(3).format(FORMATTER_DD_MMM_YYYY);
@@ -61,96 +182,6 @@ public class HistoricalNavService {
         // tp=1 Open Ended Schemes
         String historicalUrl = "https://portal.amfiindia.com/DownloadNAVHistoryReport_Po.aspx?frmdt=%s&todt=%s"
                 .formatted(fromDate, toDate);
-        Optional<MFSchemeEntity> bySchemeId = schemeService.findBySchemeCode(schemeCode);
-        String payOut;
-        boolean persistSchemeInfo = false;
-        SchemeNameAndISIN firstByAmfi = null;
-        if (bySchemeId.isEmpty()) {
-            // discontinued Scheme
-            firstByAmfi = userSchemeDetailsService
-                    .findFirstBySchemeCode(schemeCode)
-                    .orElseThrow(
-                            () -> new SchemeNotFoundException("Fund with schemeCode " + schemeCode + " Not Found"));
-            payOut = firstByAmfi.getIsin();
-            persistSchemeInfo = true;
-        } else {
-            payOut = bySchemeId.get().getPayOut();
-        }
-
-        URI uri = UriComponentsBuilder.fromHttpUrl(historicalUrl).build().toUri();
-        String oldSchemeId = null;
-        try {
-            String allNAVs = restClient.get().uri(uri).retrieve().body(String.class);
-            Reader inputString = new StringReader(Objects.requireNonNull(allNAVs));
-            try (BufferedReader br = new BufferedReader(inputString)) {
-                String lineValue = br.readLine();
-                for (int i = 0; i < 2; ++i) {
-                    lineValue = br.readLine();
-                }
-                String schemeType = lineValue;
-                String amc = lineValue;
-                while (lineValue != null && !StringUtils.hasText(oldSchemeId)) {
-                    String[] tokenize = lineValue.split(AppConstants.NAV_SEPARATOR);
-                    boolean nonAmcRow = true;
-                    boolean processRowByForce = false;
-                    if (tokenize.length == 1) {
-                        nonAmcRow = false;
-                        String tempVal = lineValue;
-                        lineValue = br.readLine();
-                        if (!StringUtils.hasText(lineValue)) {
-                            lineValue = br.readLine();
-                            tokenize = lineValue.split(AppConstants.NAV_SEPARATOR);
-                            if (tokenize.length == 1) {
-                                schemeType = tempVal;
-                                amc = lineValue;
-                            } else {
-                                amc = tempVal;
-                                processRowByForce = true;
-                            }
-                        }
-                    }
-                    if (nonAmcRow || processRowByForce) {
-                        final String schemecode = tokenize[0];
-                        final String payout = tokenize[2];
-                        if (payOut.equalsIgnoreCase(payout)) {
-                            oldSchemeId = schemecode;
-                            if (persistSchemeInfo) {
-                                String schemename = tokenize[1];
-                                String nav = tokenize[4];
-                                String date = tokenize[7];
-                                final MFSchemeDTO mfSchemeDTO = new MFSchemeDTO(
-                                        amc, Long.valueOf(schemecode), payout, schemename, nav, date, schemeType);
-                                MFSchemeEntity mfSchemeEntity =
-                                        mfSchemeDtoToEntityMapper.mapMFSchemeDTOToMFSchemeEntity(
-                                                mfSchemeDTO, mfSchemeTypeRepository);
-                                schemeService.saveEntity(mfSchemeEntity);
-                            }
-                        }
-                    }
-                    lineValue = br.readLine();
-                    if (!StringUtils.hasText(lineValue)) {
-                        lineValue = br.readLine();
-                    }
-                }
-            } catch (IOException e) {
-                LOGGER.error("Exception Occurred while reading response", e);
-                throw new NavNotFoundException("Unable to parse for %s".formatted(schemeCode), navDate);
-            }
-            if (!StringUtils.hasText(oldSchemeId) && firstByAmfi != null) {
-                // Manually creating entry in mf_scheme table as no entry found in historical link
-                MFSchemeEntity mfSchemeEntity = new MFSchemeEntity();
-                mfSchemeEntity.setPayOut(payOut);
-                mfSchemeEntity.setSchemeId(schemeCode);
-                mfSchemeEntity.setSchemeName(firstByAmfi.getScheme());
-                schemeService.saveEntity(mfSchemeEntity);
-                oldSchemeId = String.valueOf(schemeCode);
-            } else {
-                LOGGER.info("No Nav found for the given day");
-            }
-        } catch (ResourceAccessException exception) {
-            // eating as we can't do much, and it should be set when available
-            LOGGER.error("Unable to load Historical Data, downstream service is down ", exception);
-        }
-        return oldSchemeId;
+        return UriComponentsBuilder.fromHttpUrl(historicalUrl).build().toUri();
     }
 }
