610cd36a80fc5878ca2329e5e87d184f4082ac94
Raja Kolli <rajadilipkolli@gmail.com>
2024-06-10 05:52:49 +0000

    feat : checking execution of query

--- src/main/java/com/learning/mfscreener/repository/UserCASDetailsEntityRepository.java
+++ src/main/java/com/learning/mfscreener/repository/UserCASDetailsEntityRepository.java
@@ -59,11 +59,4 @@ public interface UserCASDetailsEntityRepository
                     """)
     List<PortfolioDetailsProjection> getPortfolioDetails(
             @Param("pan") String panNumber, @Param("asOfDate") LocalDate asOfDate);
-
-    @Query(
-            """
-              select u from UserCASDetailsEntity u join fetch u.folioEntities join fetch u.investorInfoEntity as i
-              where i.email = :email and i.name = :name
-              """)
-    UserCASDetailsEntity findByInvestorEmailAndName(@Param("email") String email, @Param("name") String name);
 }
