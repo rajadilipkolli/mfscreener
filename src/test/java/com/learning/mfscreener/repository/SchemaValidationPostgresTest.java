b505c55009cad31d97661562f8df8b12ce3eae87
Raja Kolli <rajadilipkolli@gmail.com>
2024-06-10 06:08:06 +0000

    fix : issue with scheme test

--- src/test/java/com/learning/mfscreener/repository/SchemaValidationPostgresTest.java
+++ src/test/java/com/learning/mfscreener/repository/SchemaValidationPostgresTest.java
@@ -2,10 +2,12 @@ package com.learning.mfscreener.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.learning.mfscreener.config.BlazePersistenceConfiguration;
 import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 
 @DataJpaTest(
         properties = {
@@ -13,6 +15,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
             "spring.test.database.replace=none",
             "spring.datasource.url=jdbc:tc:postgresql:16-alpine:///db"
         })
+@Import(BlazePersistenceConfiguration.class)
 class SchemaValidationPostgresTest {
 
     @Autowired
