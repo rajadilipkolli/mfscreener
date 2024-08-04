4ad822fa16159521ed4a5d4c15101cdca9511001
Raja Kolli <rajadilipkolli@gmail.com>
2024-08-04 13:50:47 +0530

    polish registering bean dynamically

--- src/main/java/com/learning/mfscreener/config/db/JobRunrDataSourceConfig.java
+++ src/main/java/com/learning/mfscreener/config/db/JobRunrDataSourceConfig.java
@@ -1,20 +1,17 @@
 package com.learning.mfscreener.config.db;
 
-import org.jobrunr.spring.autoconfigure.storage.JobRunrSqlStorageAutoConfiguration;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
-import org.springframework.boot.autoconfigure.AutoConfigureBefore;
-import org.springframework.boot.autoconfigure.jdbc.JdbcConnectionDetails;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.lang.NonNull;
 
 @Configuration(proxyBeanMethods = false)
-@AutoConfigureAfter(JdbcConnectionDetails.class)
-@AutoConfigureBefore(JobRunrSqlStorageAutoConfiguration.class)
+@AutoConfigureAfter(DataSourceAutoConfiguration.class)
 class JobRunrDataSourceConfig implements BeanFactoryPostProcessor {
 
     @Override
