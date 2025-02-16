b5fec43bb38697d43b754b4a2e46c69008cf6f52
Raja Kolli <rajadilipkolli@gmail.com>
2025-02-16 15:44:22 +0530

    Update SchemeControllerIT.java

--- src/test/java/com/learning/mfscreener/web/controllers/SchemeControllerIT.java
+++ src/test/java/com/learning/mfscreener/web/controllers/SchemeControllerIT.java
@@ -31,6 +31,6 @@ class SchemeControllerIT extends AbstractIntegrationTest {
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(header().string(HttpHeaders.CONTENT_TYPE, is(MediaType.APPLICATION_JSON_VALUE)))
-                .andExpect(jsonPath("$.size()", is(213)));
+                .andExpect(jsonPath("$.size()", is(217)));
     }
 }
