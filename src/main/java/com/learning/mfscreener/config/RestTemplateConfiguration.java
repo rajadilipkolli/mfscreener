0750d267d581ebdf2903e5ef8ec8e96c0ec7d3ac
Raja Kolli <rajadilipkolli@gmail.com>
2025-11-25 10:28:01 +0000

    polish rest client configuration

--- src/main/java/com/learning/mfscreener/config/RestTemplateConfiguration.java
+++ src/main/java/com/learning/mfscreener/config/RestTemplateConfiguration.java
@@ -1,27 +1,16 @@
 package com.learning.mfscreener.config;
 
-import java.time.Duration;
-import org.springframework.boot.restclient.RestTemplateBuilder;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
-import org.springframework.web.client.RestTemplate;
 
 @Configuration(proxyBeanMethods = false)
 @EnableCaching
 public class RestTemplateConfiguration {
 
     @Bean
-    RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
-        return restTemplateBuilder
-                .connectTimeout(Duration.ofSeconds(30))
-                .readTimeout(Duration.ofSeconds(60))
-                .build();
-    }
-
-    @Bean
-    RestClient restClient(RestTemplate restTemplate) {
-        return RestClient.create(restTemplate);
+    RestClient restClient(RestClient.Builder builder) {
+        return builder.build();
     }
 }
