52884742c88574fe79055c57abfa065e0c0053fb
Raja Kolli <rajadilipkolli@gmail.com>
2025-12-26 11:41:54 +0530

    Update observability dependencies in build.gradle (#603)
    * Update observability dependencies in build.gradle
    
    Removed Zipkin and Micrometer tracing dependencies, added OpenTelemetry starter.
    
    * Update test dependencies in build.gradle
    
    Replaced Micrometer and Zipkin test dependencies with OpenTelemetry and Grafana test dependencies.
    
    * Update expected links count in actuator test
    
    * Add LgtmStackContainer configuration
    
    * Add RestartScope annotation to lgtmContainer bean

--- src/test/java/com/learning/mfscreener/common/NonSQLContainersConfig.java
+++ src/test/java/com/learning/mfscreener/common/NonSQLContainersConfig.java
@@ -1,10 +1,13 @@
 package com.learning.mfscreener.common;
 
 import com.redis.testcontainers.RedisContainer;
+import java.time.Duration;
 import org.springframework.boot.devtools.restart.RestartScope;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
+import org.testcontainers.grafana.LgtmStackContainer;
+import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
 public class NonSQLContainersConfig {
@@ -14,5 +17,13 @@ public class NonSQLContainersConfig {
     @RestartScope
     RedisContainer redisContainer() {
         return new RedisContainer(RedisContainer.DEFAULT_IMAGE_NAME.withTag("8.4.0-alpine"));
+    }
+
+    @Bean
+    @ServiceConnection
+    @RestartScope
+    LgtmStackContainer lgtmContainer() {
+        return new LgtmStackContainer(DockerImageName.parse("grafana/otel-lgtm:0.13.0"))
+                .withStartupTimeout(Duration.ofMinutes(2));
     }
 }
