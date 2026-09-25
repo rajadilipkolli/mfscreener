99235087603241be19a5f44c09d230f9ef3a680a
Raja Kolli <rajadilipkolli@gmail.com>
2026-09-25 19:17:00 +0000

    fix : spotless issue

--- src/main/java/com/learning/mfscreener/repository/UserCASDetailsEntityRepository.java
+++ src/main/java/com/learning/mfscreener/repository/UserCASDetailsEntityRepository.java
@@ -6,7 +6,6 @@ import java.time.LocalDate;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.NativeQuery;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
